f5bbfd9df19e742c0982922f6aafd1edac4ab286
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-04-23 13:56:13 -0300

    Tela1 60%, quizzes listados e identificados, falta completar a função que acessa cada um e a função que localiza os quizzes do usuário

--- script1.js
+++ script1.js
@@ -1,18 +1,27 @@
 axios.defaults.headers.common['Authorization'] = 'EAfOvX9dRBbHsFCJzme9yXNE'; 
 
 let quizzes = [];
+function Play_quizz(identificador) {
+    alert("Vamos jogar o quizz de ID: " + identificador);
+}
+function povoar_quizzes() {
+    const localização = document.querySelector(".todos_os_quizzes");
+    for (let i = 0; i < quizzes.length; i++) {
+        let div_adicional = `
+        <div onclick="Play_quizz(${quizzes[i].id})" class="Quiz_individual">
+            <img  class="Img_quiz" src=${quizzes[i].image} alt="">
+            <div class="Titulo_quiz_individual">${quizzes[i].title}</div>
+
+        `;
+        localização.innerHTML += div_adicional;
+    }
+}
 function analise_quizzes(resposta) {
     quizzes = resposta.data;
+    povoar_quizzes();
 }
 function coletar_quizzes() {
     const promisse_quizzes = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
     promisse_quizzes.then(analise_quizzes);
 }
 coletar_quizzes();
-
-function povoar_quizzes() {
-    const localização = document.querySelector(".todos_os_quizzes");
-    for (let i = 0; i < quizzes.length; i++) {
-        let div_adicional;
-    }
-}
